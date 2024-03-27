e8bd461c5cd2a21837f74430d37fba335f3973c5
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-27 15:24:09 +0530

    update

--- ci.sh
+++ ci.sh
@@ -1,4 +1,5 @@
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/browny.zip
 unzip browny.zip
 mv browny-v1.0 browny
-aws s3 cp -r browny s3://browny-demo/
+rm -rf browny.zip
+aws s3 cp browny s3://browny-demo/browny --recursive
