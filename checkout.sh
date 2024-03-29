94c711d8f19fac135cccfde8bac0403bccbf5e02
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-29 22:16:28 +0530

    update

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,2 @@
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/browny.zip
-unzip browny.zip
-mv browny-v1.0 browny
-aws s3 cp -r browny s3://browny-demo/
+aws s3 cp -r browny.zip s3://browny-demo/
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 aws s3 cp s3://browny-demo/ . --recursive
+unzip browny.zip
 sudo rm  /usr/share/httpd/noindex/*
-sudo cp -r browny/* /usr/share/httpd/noindex/
+sudo cp -r browny-v1.0 /* /usr/share/httpd/noindex/
