6ce32a71c675fe2cd4a3799f54a1d4391158765c
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-29 23:27:09 +0530

    update

--- nginx-cd.sh
+++ nginx-cd.sh
@@ -1,4 +1,4 @@
 aws s3 cp s3://browny-demo/ . --recursive
 unzip browny.zip
-sudo rm -rf /usr/share/httpd/noindex/*
-sudo cp browny-v1.0/* /usr/share/httpd/noindex/ --recursive
+sudo rm -rf /usr/share/nginx/html/*
+sudo cp browny-v1.0/* /usr/share/nginx/html/ --recursive
