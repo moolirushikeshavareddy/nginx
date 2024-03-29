3e8e33866a1930e9992e2aaeb30357d1186b5d50
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-29 21:28:36 +0530

    update

--- checkout.sh
+++ checkout.sh
@@ -1,6 +1,6 @@
+cd /opt/
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/browny.zip
 unzip browny.zip
 mv browny-v1.0 browny
 rm -rf browny.zip
 aws s3 cp browny s3://browny-demo/ --recursive
-rm -rf browny
