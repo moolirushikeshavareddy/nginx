2bbb30faa3807a541b3442d38b268eb60912fd53
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-29 22:40:27 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 aws s3 cp s3://browny-demo/ . --recursive
 unzip browny.zip
-sudo rm  /usr/share/httpd/noindex/*
-sudo cp browny-v1.0 /* /usr/share/httpd/noindex/ --recursive
+sudo rm /usr/share/httpd/noindex/*
+sudo cp browny-v1.0/* /usr/share/httpd/noindex/ --recursive
