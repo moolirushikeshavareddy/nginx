db2fb07a33fc8a7173ab297bbd65099b05a4631a
Rushi Keshava Reddy Mooli <rushikeshavareddy77@gmail.com>
2024-03-29 22:36:52 +0530

    update

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,2 @@
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/browny.zip
-pwd
-ls -lrta
-aws s3 cp browny.zip s3://browny-demo/ --recursive
+aws s3 cp browny.zip s3://browny-demo/
